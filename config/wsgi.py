d0561eb0ea05235e3d07e1ea0ba1241f6dd4e6c6
Nik Kantar <nik@nkantar.com>
2020-06-23 22:44:41 -0700

    Black all the things

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'microblot.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microblot.settings")
 
 application = get_wsgi_application()
