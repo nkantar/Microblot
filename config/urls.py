2ee02a0fc4c2cb1b2ecfee7ad4871a7af430e682
Nik Kantar <nik@nkantar.com>
2020-06-23 22:51:46 -0700

    Clean up some files

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path("api/", include("microblot.api.urls")),
     path("admin/", admin.site.urls),
--- microblot/api/urls.py
+++ microblot/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 
-# from .slack import urls as slack_urls
 
 urlpatterns = [
     path("slack/", include("microblot.api.slack.urls")),
