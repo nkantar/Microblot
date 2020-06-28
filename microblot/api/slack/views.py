a44efd8c77eec8b91310c8b2221fff973de058f7
Nik Kantar <nik@nkantar.com>
2020-06-28 15:17:01 -0700

    Add WIP `/microblot new` implementation
    This doesn't really work yet.
    
    The modal fires nicely and looks lovely, but submitting it does…well,
    nothing, really. The spinny circle keeps spinning and spinning and
    spinning and eventually "We had some trouble connecting." shows up.
    In the meantime, there are no requests to the app, so either something
    is misconfigured, or I'm misunderstanding how this is supposed to work.
    
    To be fixed. >_>

--- microblot/api/slack/views.py
+++ microblot/api/slack/views.py
@@ -1,12 +1,16 @@
+import hashlib
+import hmac
 import json
 
 import requests
 
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from microblot.blogs.models import Blog
+
+from .modals import NEW_POST
 
 
 def ping(request):
@@ -49,8 +53,22 @@ def slack_oauth(request):
     return HttpResponse("oauth")  # TODO #19
 
 
-def slack_new():
-    return HttpResponse("new")  # TODO #20
+def slack_new(request):
+    blog = Blog.objects.get(platform__team_id=request.POST["team_id"])
+    views_open_url = f"{settings.SLACK_API_ROOT_URL}/views.open"
+    new_modal_response = requests.post(
+        views_open_url,
+        data={
+            "token": blog.platform["bot_access_token"],
+            "trigger_id": request.POST["trigger_id"],
+            "view": NEW_POST,
+            "callback_id": "foobar",
+        },
+    )
+
+    # TODO: switch to Slack Python SDK, maybe that'll help? #50
+
+    return JsonResponse({"status_code": 200})
 
 
 def slack_list():
@@ -83,10 +101,31 @@ def slack_download():
 def dispatch(request):
     # TODO #25
 
-    text = request.GET.get("text")
+    breakpoint()
+
+    slack_signature = request.headers["X-Slack-Signature"]
+
+    request_timestamp = request.headers["X-Slack-Request-Timestamp"]
+    request_body = request.body.decode()
+    message = f"v0:{request_timestamp}:{request_body}"
+    hashed_message = hmac.new(
+        bytes(settings.SLACK_SIGNING_SECRET, "utf-8"),
+        bytes(message, "utf-8"),
+        digestmod=hashlib.sha256,
+    ).hexdigest()
+    signature = f"v0={hashed_message}"
+
+    signatures_match = hmac.compare_digest(slack_signature, signature)
+    if not signatures_match:
+        return HttpResponseForbidden
+
+    text = request.POST.get("text")
     params = text.split(maxsplit=1)
     command = params[0]
     func = globals().get(f"slack_{command}")
 
+    print(command)
+    breakpoint()
+
     # TODO #24
-    return func(*(params[1:]))
+    return func(request)
