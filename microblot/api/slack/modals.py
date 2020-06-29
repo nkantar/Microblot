09eda58c7a02969b1e98e5cea982bc55e95700db
Nik Kantar <nik@nkantar.com>
2020-06-28 22:19:34 -0700

    Update Slack modal

--- microblot/api/slack/modals.py
+++ microblot/api/slack/modals.py
@@ -1,50 +1,58 @@
-NEW_POST = {
-    "type": "modal",
-    "callback_id": "modal-id",
-    "title": {"type": "plain_text", "text": "Microblot: New Post"},
-    "submit": {"type": "plain_text", "text": "Submit"},
-    "close": {"type": "plain_text", "text": "Cancel"},
-    "blocks": [
-        {
-            "type": "input",
-            "block_id": "post_title_id",
-            "element": {
-                "type": "plain_text_input",
-                "action_id": "post_title_action_id",
-                "placeholder": {"type": "plain_text", "text": "Witty title goes here",},
+import json
+
+
+NEW_POST = json.dumps(
+    {
+        "type": "modal",
+        "callback_id": "modal-id",
+        "title": {"type": "plain_text", "text": "Microblot: New Post"},
+        "submit": {"type": "plain_text", "text": "Submit"},
+        "close": {"type": "plain_text", "text": "Cancel"},
+        "blocks": [
+            {
+                "type": "input",
+                "block_id": "post_title_id",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "post_title_action_id",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Witty title goes here",
+                    },
+                },
+                "label": {"type": "plain_text", "text": "Title"},
             },
-            "label": {"type": "plain_text", "text": "Title"},
-        },
-        {
-            "type": "input",
-            "block_id": "post_body_md_id",
-            "element": {
-                "type": "plain_text_input",
-                "action_id": "post_body_md_action_id",
-                "multiline": True,
-                "placeholder": {
+            {
+                "type": "input",
+                "block_id": "post_body_md_id",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "post_body_md_action_id",
+                    "multiline": True,
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Mind blowing post goes here",
+                    },
+                },
+                "label": {"type": "plain_text", "text": "Body"},
+                "hint": {
                     "type": "plain_text",
-                    "text": "Mind blowing post goes here",
+                    "text": "GitHub Flavored Markdown supported",
                 },
             },
-            "label": {"type": "plain_text", "text": "Body"},
-            "hint": {
-                "type": "plain_text",
-                "text": "GitHub Flavored Markdown supported",
-            },
-        },
-        {
-            "type": "input",
-            "block_id": "post_category_id",
-            "element": {
-                "type": "plain_text_input",
-                "action_id": "post_category_action_id",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "Sensible category goes here",
+            {
+                "type": "input",
+                "block_id": "post_category_id",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "post_category_action_id",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Sensible category goes here",
+                    },
                 },
+                "label": {"type": "plain_text", "text": "Category"},
             },
-            "label": {"type": "plain_text", "text": "Category"},
-        },
-    ],
-}
+        ],
+    }
+)
