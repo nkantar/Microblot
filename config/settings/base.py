e3373d556f55b0e71d44f3a307a1fa661b16747c
Nik Kantar <nik@nkantar.com>
2021-01-13 20:48:53 -0800

    Fix env var name typo

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 
 # Application definition
 
