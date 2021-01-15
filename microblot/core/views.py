f4196452f0a04ca24302bfc599119cb0e7b196e9
Nik Kantar <nik@nkantar.com>
2021-01-15 10:55:13 -0800

    Add docstring to dispatch view function

--- microblot/core/views.py
+++ microblot/core/views.py
@@ -3,6 +3,15 @@ from django.http import Http404
 
 
 def dispatch(request, main_class=None, cms_class=None, short_class=None, **kwargs):
+    """
+    Delegate to the correct class based on the current site.
+
+    This helper view allows the user to delegate the request to the correct class based
+    on the current site. This allows for different apps with their own needs to "share"
+    URLs when appropriate.
+
+    Each of the "_class" params corresponds to a view class for the desired app.
+    """
     classes = {
         settings.FULL_DOMAIN: main_class,
         f"www.{settings.FULL_DOMAIN}": main_class,
