07e564f8a6086a22ba38ccc3e4a8f03edc790532
Nik Kantar <nik@nkantar.com>
2021-01-26 20:37:09 -0800

    Refactor for readability

--- microblot/slack/actionables.py
+++ microblot/slack/actionables.py
@@ -4,18 +4,8 @@ from .models import SlackWorkspace
 from .modals import populate_post_modal
 
 
-def command_new(
-    team_id,
-    user_id,
-    action,
-    params,
-    token,
-    response_url,
-    trigger_id,
-):
-
+def command_new(team_id, user_id, action, params, token, response_url, trigger_id):
     workspace = SlackWorkspace.objects.get(slack_id=team_id)
-
     client = WebClient(token=workspace.bot_access_token)
     response = client.views_open(
         trigger_id=trigger_id,
