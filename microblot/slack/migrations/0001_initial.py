5f7bb7ecbd8bb3e2a26c5b4f7b16ef7716f1ee68
Nik Kantar <nik@nkantar.com>
2021-01-27 23:40:07 -0800

    Format migration

--- microblot/slack/migrations/0001_initial.py
+++ microblot/slack/migrations/0001_initial.py
@@ -9,43 +9,75 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('cms', '0001_initial'),
+        ("cms", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='SlackWorkspace',
+            name="SlackWorkspace",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('slack_id', models.CharField(max_length=16)),
-                ('icon', models.CharField(max_length=512)),
-                ('bot_user_id', models.CharField(max_length=128)),
-                ('bot_access_token', models.CharField(max_length=128)),
-                ('blog', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='cms.blog')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("slack_id", models.CharField(max_length=16)),
+                ("icon", models.CharField(max_length=512)),
+                ("bot_user_id", models.CharField(max_length=128)),
+                ("bot_access_token", models.CharField(max_length=128)),
+                (
+                    "blog",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="cms.blog"
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='SlackUser',
+            name="SlackUser",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('slack_id', models.CharField(max_length=16)),
-                ('icon', models.CharField(max_length=512)),
-                ('slack_username', models.CharField(max_length=128)),
-                ('real_name', models.CharField(max_length=128)),
-                ('display_name', models.CharField(max_length=128)),
-                ('avatar', models.CharField(max_length=512)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='cms.author')),
-                ('workspace', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='slack.slackworkspace')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("slack_id", models.CharField(max_length=16)),
+                ("icon", models.CharField(max_length=512)),
+                ("slack_username", models.CharField(max_length=128)),
+                ("real_name", models.CharField(max_length=128)),
+                ("display_name", models.CharField(max_length=128)),
+                ("avatar", models.CharField(max_length=512)),
+                (
+                    "author",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="cms.author"
+                    ),
+                ),
+                (
+                    "workspace",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="slack.slackworkspace",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
